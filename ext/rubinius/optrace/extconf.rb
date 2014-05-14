12e2f3767cf36dca998ae56a6b6d1052cb9b2e7e
Gabriel Southern <gsouther@soe.ucsc.edu>
2014-05-14 14:29:52 -0700

    Set compiler flags for building extension
    Use CC and CXX environment variables to pick compiler when
    building extension.
    
    Set -std=c++11 to support C++11 compilation mode.

--- ext/rubinius/optrace/extconf.rb
+++ ext/rubinius/optrace/extconf.rb
@@ -1,2 +1,7 @@
 require 'mkmf'
+
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
+RbConfig::MAKEFILE_CONFIG['CXXFLAGS'] << ' -std=c++11'
+
 create_makefile 'rubinius/optrace/optrace'
