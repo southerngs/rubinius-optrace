ee6b06a1c5a754861db8030872b6274976b49126
Gabriel Southern <gsouther@soe.ucsc.edu>
2014-05-13 17:33:45 -0700

    fix crash if stop called before start

--- lib/rubinius/optrace.rb
+++ lib/rubinius/optrace.rb
@@ -33,23 +33,29 @@ module Rubinius
 
     def start
       Rubinius::Tooling.enable
+      @started = true
       self
     end
 
     def stop
-      @results = Rubinius::Tooling.disable
+      if @started then
+        @results = Rubinius::Tooling.disable
+        @started = false
+        @stopped = true
+      end
       self
     end
 
     #TODO: This is slow. Might want to optimize or memoize the result
     def trace 
-      trace = Array.new
+      return nil unless @stopped
+      trace = nil
       @results.each do |r|
         ip = r[0]
         trace << r[1].decode.select { |t| t.ip == ip }
       end
       trace
     end
-
   end
 end
+
